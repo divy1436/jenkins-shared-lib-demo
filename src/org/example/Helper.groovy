30d3fd2e2628373a42c4973ebdc5931741286347
Divyanshu Maurya <2024pceacsdivyanshu58@poornima.org>
2025-12-25 14:52:06 +0530

    Fix spelling of variable in toUpperCase method
    Corrected spelling of variable 'name' to 'n'.

--- src/org/example/Helper.groovy
+++ src/org/example/Helper.groovy
@@ -2,7 +2,7 @@ package org.example
 
 class Helper { 
   static String toUpperCase(String input) {
-    def n = name ?: 'there'
+    def n = name ?: 'there'  // Corrected spelling
     return input.toUpperCase()
     
   }
