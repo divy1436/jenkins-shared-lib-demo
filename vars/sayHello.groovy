880f735c2373c545f0887046baf1afadbee30873
Divyanshu Maurya <2024pceacsdivyanshu58@poornima.org>
2025-12-25 15:22:35 +0530

    Update greeting retrieval method in sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,6 +1,7 @@
 import org.example.Helper
 
 def call(String name = 'there'){
- def greeting = Helper.readGreeting()
+def greeting = libraryResource('org/example/greeting,txt').trim()
+ def upperName = Helper.readGreeting()
  echo "${greeting} $(upperName}!"
 }
