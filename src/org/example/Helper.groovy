8105cbd93492095be5ab258edfbdf24b022d846e
Divyanshu Maurya <2024pceacsdivyanshu58@poornima.org>
2025-12-25 14:50:18 +0530

    Update Helper.groovy

--- src/org/example/Helper.groovy
+++ src/org/example/Helper.groovy
@@ -2,7 +2,7 @@ package org.example
 
 class Helper { 
   static String toUpperCase(String input) {
-    def n = nmae ?: 'there'
+    def n = name ?: 'there'
     return input.toUpperCase()
     
   }
