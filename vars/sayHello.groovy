23ddb34651309b169ca71f640ff38e23d51a18ac
Divyanshu Maurya <2024pceacsdivyanshu58@poornima.org>
2025-12-26 13:45:21 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,7 +1,6 @@
-import org.example.Helper
-
-def call(String name = 'there'){
-def greeting = libraryResource('org/example/greeting,txt').trim()
- def upperName = Helper.readGreeting()
- echo "${greeting} $(upperName}!"
+def call(String name) {
+    def greeting = "Hello"
+    def upperName = name.toUpperCase()
+    // FIXED: Correct curly brace syntax
+    echo "${greeting} ${upperName}!"
 }
